98740c30c431fe35df90095096f2cff670e64b19
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-05-16 20:12:06 +0200

    feat!: initial mod

---
+++ code/src/content/expose_singlepress_inputaction.entity.patch.ts
@@ -0,0 +1,16 @@
+import { getPath, IPath, type QNPatch } from 'quickentity-script'
+
+export const TARGET: IPath = { ...getPath('[assembly:/_pro/design/gamecore/setpiecehelpers.template?/setpiecehelpers_activator_singlepress.entitytemplate]') }
+export const NAME: string = 'expose_singlepress_inputaction'
+
+export async function create(patch: QNPatch, option?: string) {
+  patch
+    .getEntity('cecdc1465dd13fbb')
+    .addPropertyAliasConnection(
+      'm_eInputAction',
+      {
+        originalProperty: 'm_eInputAction',
+        originalEntity: 'b6dba39a251bfef1'
+      }
+    )
+}
